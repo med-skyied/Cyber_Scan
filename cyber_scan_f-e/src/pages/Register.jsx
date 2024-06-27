5c649ee69bf97a59f1cc03af13e33a7f48929536
amch-med23 <skyied.med2023@gmail.com>
2024-06-27 17:44:44 -0400

    adding the first version of the Register page

--- cyber_scan_f-e/src/pages/Register.jsx
+++ cyber_scan_f-e/src/pages/Register.jsx
@@ -3,10 +3,28 @@ import { Link } from 'react-router-dom';
 
 import NavBarChild from '../components/NavBarChild';
 function Register() {
+    function register_handler() {
+        console.log('auth register handlere is called...');
+    }
  return (
     <>
         <NavBarChild />
-        <p>This is the register page.</p>
+        
+        <div className='auth_container'>
+            <div className='auth_form_holder'>
+                <form onSubmit={register_handler()}>
+                    <p>Enter your email:</p>
+                    <input className='input_field' type='email' placeholder='email' id='user_email' required></input>
+                    <p>Enter your password:</p>
+                    <input className='input_field' type='password'  id='def_password' required placeholder='password'></input>
+                    <p>Re-Enter your password:</p>
+                    <input className='input_field' type='password'  id='def_password_conf' required placeholder='password confirmation'></input>
+                    <br/> <br />
+                    <button type='submit' className='authSubmitButton'>Register</button>
+                    <Link className='to_link' to='/login'>or Login</Link>
+                </form>
+            </div>
+        </div>
     </>
  )
 }

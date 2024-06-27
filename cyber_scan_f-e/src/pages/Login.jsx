dcfe019f1d49529ebaf54e4096a05018ee30e5af
amch-med23 <skyied.med2023@gmail.com>
2024-06-27 17:44:22 -0400

    adding the first version of the Login page (this first version of login/register has only the html side of things defined, but not the logic. this will come later)

--- cyber_scan_f-e/src/pages/Login.jsx
+++ cyber_scan_f-e/src/pages/Login.jsx
@@ -1,12 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import AuthBackground from '../assets/auth_background.jpg'
 
 import NavBarChild from '../components/NavBarChild';
 function Login() {
+
+    function login_handler() {
+        console.log('auth login handlere is called...');
+    }
  return (
     <>
     <NavBarChild/>
-        <p>This is the log in page.</p>
+
+        <div className='auth_container'
+        
+        /*style={{ backgroundImage : `url(${AuthBackground})`,
+        backgroundRepeat: 'no-repeat',
+        height:'100vh',
+        width: '100%',
+        objectFit: 'cover'}}*/
+        >
+            <div className='auth_form_holder' >
+                <form onSubmit={login_handler()}>
+                    <p>Enter your email:</p>
+                    <input className='input_field' type='email' placeholder='email' id='user_email' required></input>
+                    <p>Enter your password:</p>
+                    <input className='input_field' type='password'  id='user_password' required placeholder='password'></input>
+                    <br/> <br />
+
+                    <button type='submit' className='authSubmitButton'>Log in</button>
+                    <br /> <br />
+                    <Link to='/reset_password'>Reset your password</Link>
+                    <Link className='to_link' to='/register'>or Register new account</Link>
+                </form>
+
+            </div>
+        </div>
     </>
  )
 }

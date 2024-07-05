e1fa721f4149147cb5e2674ddec6dab3f238dfc6
amch-med23 <skyied.med2023@gmail.com>
2024-07-05 09:25:42 -0400

    adding the request handelling logic for the CVEs page tool

--- cyber_scan_f-e/src/services/CVEs_pages.jsx
+++ cyber_scan_f-e/src/services/CVEs_pages.jsx
@@ -13,9 +13,18 @@ export default function CVEsSearch() {
         <>
             <NavBarChild />
             <div className="main_container">
-                <div className="find_my_ip">
-                    <p>Enter the year to see the cves that are there</p>
-
+                <div className="cves_pages">
+                    <div className="cve_inner_holder">
+                        <p>This Service permitts you to explore the CVEs (common vulnerability Exposures)
+                            known also as software exploits, that have been designed and exploiated in the past.</p>
+                        <p>Some of the systems online are still affected by these vulnerabilities even though they are patched (some new ones might not be patched yet).
+                            So be careafull and responsible, We trust you with this.</p>
+                           <form className="cve_input_form">
+                                <p>Provide the year the desired CVEs were introduced in (1999-the  current year)</p>
+                                <input required id="cves_year"></input><span> use only numbers</span><br/> <br/>
+                                <button type="submit">Search</button>
+                            </form>
+                        </div>
                     
 
                 </div>

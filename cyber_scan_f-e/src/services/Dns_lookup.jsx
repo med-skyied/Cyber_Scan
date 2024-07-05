d1d7ff3ce41bb9cf9235cf2c536ca510e691e716
amch-med23 <skyied.med2023@gmail.com>
2024-07-05 09:26:07 -0400

    adding the main logic for the Dns lookup tool

--- cyber_scan_f-e/src/services/Dns_lookup.jsx
+++ cyber_scan_f-e/src/services/Dns_lookup.jsx
@@ -14,9 +14,14 @@ export default function DnsLookUp() {
             <NavBarChild />
             <div className="main_container">
                 <div className="dns_lookup">
-                    <p>inter the dns to look it up in our systems</p>
-
-                    
+                <div className='dns_lookup_container'>
+                  <form className='form_dnslookup'>
+                      <p>Enter the domain name to retreive it's dns Type 'A' record (Used to retreive the ip address of a domain name)</p>
+                      <input id='domain name' required></input> <span>Only one domain name at a time</span> <br/><br/>
+                      <button type='submit'>Look Up</button>
+                  </form>
+                </div>
+                  <div className="dns_lookup_res"> the result</div>
                 </div>
             </div>
             <Footer />

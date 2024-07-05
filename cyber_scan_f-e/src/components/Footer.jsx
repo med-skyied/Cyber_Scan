36daf39d47adf802b9615c38676e63d62a3495c1
amch-med23 <skyied.med2023@gmail.com>
2024-07-05 09:29:03 -0400

    implementing the system status front-end logic for the Footer component

--- cyber_scan_f-e/src/components/Footer.jsx
+++ cyber_scan_f-e/src/components/Footer.jsx
@@ -1,17 +1,46 @@
 import React from "react";
 import '../App.css'
-
+import axios from 'axios';
 
 function Footer() {
 
     function sys_status() {
-        
+        const apiEndPoint = 'http://localhost:5000/api/v1/';
+        axios.get(apiEndPoint.concat('/status')).then(res => {
+          //api responds correctelly
+          let status = res.data['sys_status']
+          //console.log(status)
+          if (status === "Success"){
+            // set the status to sustem is okay
+            //console.log('inside success')
+            let status_parag = document.getElementById('status_holder');
+            status_parag.innerHTML = '<span class="system_status_ok" style="color: #00FF00;">&nbsp;All systems are operational.</span>'
+            let dot_holder = document.getElementById('status_dot');
+            dot_holder.innerHTML = '<div class="green-dot"></div>';
+          }
+          else {
+            console.log("api errors were found");
+            let status_parag = document.getElementById('status_holder');
+            status_parag.innerHTML = '<span class="system_status_failed" style="color: red">&nbsp;API endpoints are not responding.</span>' ;
+            let dot_holder = document.getElementById('status_dot');
+            dot_holder.innerHTML = '<div class="red-dot"></div>';
+          }
+        }).catch(error => {
+          //erors in the api
+          console.log("api errors were found");
+          let status_parag = document.getElementById('status_holder');
+          status_parag.innerHTML = '<span class="system_status_failed" style="color: red">&nbsp;API endpoints are not responding.</span>' ;
+          let dot_holder = document.getElementById('status_dot');
+          dot_holder.innerHTML = '<div class="red-dot"></div>';
+        })
     }
 
     return (
         <>
+          {sys_status()}
             <div className="Footer">
-                <p>System Status: </p>
+
+                <span className='sys_status_txt'>System Status: </span> <div id='status_dot'></div><div id='status_holder'></div>
 
             </div>
         </>

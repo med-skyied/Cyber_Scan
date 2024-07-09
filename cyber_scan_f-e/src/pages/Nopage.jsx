5fd892679917eea4b4579646ee2bf39c7b5bc19c
amch-med23 <skyied.med2023@gmail.com>
2024-07-09 15:37:37 -0400

    inhancing the Nopage to contains the newelly added navbar element.

--- cyber_scan_f-e/src/pages/Nopage.jsx
+++ cyber_scan_f-e/src/pages/Nopage.jsx
@@ -2,14 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import NavBarChild from '../components/NavBarChild';
+import NavBarChildLogin from '../components/NavBarChildLogin';
+
 function Nopage() {
+
+  let user_authenticated = false;
+
+  function userAuthState(){
+    if (user_authenticated){
+      return (<NavBarChildLogin />)
+    } else {
+      return (<NavBarChild />)
+    }
+  }
+
  return (
     <>
-        <NavBarChild />
+        {userAuthState()}
         <div className='main_container'>
             <p>404.</p>
         </div>
-        
+
     </>
  )
 }

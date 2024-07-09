9b66b0d59dba2b6e50bbb52ff09128319c89fcc1
amch-med23 <skyied.med2023@gmail.com>
2024-07-09 15:36:11 -0400

    inhancing the contact page, but this addresses minor issues only (we still have more layouts to add, we will go with siple view)

--- cyber_scan_f-e/src/pages/Contact.jsx
+++ cyber_scan_f-e/src/pages/Contact.jsx
@@ -2,16 +2,28 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import NavBarChild from '../components/NavBarChild';
+import NavBarChildLogin from '../components/NavBarChildLogin';
 
 function Contact() {
+
+  let user_authenticated = false;
+
+  function userAuthState(){
+    if (user_authenticated){
+      return (<NavBarChildLogin />)
+    } else {
+      return (<NavBarChild />)
+    }
+  }
+
  return (
     <>
-        <NavBarChild />
+        {userAuthState()}
         <div className='main_container'>
         <div className='contact_container'>
             <p>You can contact us via this email: <span><a href='mailto:skyied.med2023@gmail.com' > skyied.med2023@gmail.com </a></span> </p>
             <p>Or you can talk to me on my socials: /.my socials./</p>
-            
+
             </div>
         </div>
 

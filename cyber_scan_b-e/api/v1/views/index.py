d3dc318ce2278b57e1bd98ada779057f846bbe2c
amch-med23 <skyied.med2023@gmail.com>
2024-07-09 15:47:27 -0400

    adding the required routes for the app models

--- cyber_scan_b-e/api/v1/views/index.py
+++ cyber_scan_b-e/api/v1/views/index.py
@@ -7,10 +7,19 @@ import json
 import uuid
 import requests
 
+
 # importing other models as needed bellow
 from api.v1.models.system_check import check_status
 from api.v1.models.security_risk_level import sec_risk_level
 from api.v1.models.target_info_gathering import gather_target_info
+from api.v1.models.shodan_dns_lookup import dns_lookup_details
+from api.v1.models.forensic_ip_history import ip_address_history
+from api.v1.models.target_port_scanner import scan_port
+from api.v1.models.cve_search import vendors_affected_products
+from api.v1.models.cve_search import vendors_affected # for afected vendors
+from api.v1.models.cve_search import latest_30_new_cves
+from api.v1.models.cve_search import search_by_cve_id
+from api.v1.models.cve_search import get_cves_db_info
 
 @app_views.route('/status', methods=['GET'],
                 strict_slashes=False)
@@ -49,3 +58,107 @@ def target_data_gathering():
 
     # returning the proper data as returnd from the model
     return make_response(result_obj, 200)
+
+@app_views.route('/dns_lookup', methods=['POST'],
+                strict_slashes=False)
+def dns_lookup_view():
+    """ retreives the dns type A of a given host name """
+    if not request.is_json:
+        abort(404, 'Not a JSON');
+    provided_domain_name = []
+    provided_domain_name.append(request.get_json()['domain_name']) # this was done because this function accepts a list of arguments
+    #print(f'we have got: {provided_domain_name}')
+    returned_data = dns_lookup_details(provided_domain_name)
+    #print(f'we are returning: {returned_data}')
+    return make_response(returned_data, 200)
+
+@app_views.route('/ip_history', methods=['POST'],
+                strict_slashes=False)
+def ip_history():
+    """ get a target ip address history """
+
+    if not request.is_json:
+        abort(404, 'Not a json')
+    provided_host_name = request.get_json()['target_domain_name']
+
+    # data processing was doneat the model level.
+    returned_obj = ip_address_history(provided_host_name)
+
+    return make_response(returned_obj, 200) # returning the object directely as it is from the model,
+    # this means that we thust the data processing at the model level.
+
+@app_views.route('/port_scanning', methods=['POST'],
+                strict_slashes=False)
+def port_scan():
+    """ scan a target ip / host name for open ports """
+    if not request.is_json:
+        abort(404, 'Not a Json')
+    target_host = request.get_json()['target_host']
+
+    # processing the data via the corresponding model.
+    returned_result = scan_port(target_host)
+
+    #returning the resulted data.
+    return make_response(returned_result, 200)
+
+# This is for the CVEs endpoints.
+# for these CVEs end-points we must check the boelean value of the returnd value first.
+@app_views.route('/vendors_affected', methods=['GET'],
+                strict_slashes=False)
+def exploited_vendors():
+    """ explore the past exploited Vendors """
+    ret_vendors = vendors_affected() # retrieved vendors
+    if (ret_vendors):
+        return make_response(ret_vendors, 200)
+    else:
+        return make_response(jsonify({'error': 'Internal Server Error'}), 500)
+
+
+@app_views.route('/vendors_affected_products', methods=['POST'],
+                strict_slashes=False)
+def targeted_vndr_prdct():
+    """ explore the previouslyy exploited products of a given vendor. """
+    if not request.is_json:
+        abort(404, 'Not a json')
+    supplied_vendor_name = request.get_json()['vendor_name']
+    print(supplied_vendor_name)
+    vendors_affecte_prods = vendors_affected_products(supplied_vendor_name)
+    if (vendors_affecte_prods):
+        return make_response(vendors_affecte_prods, 200) # the data was already returned in json
+    else:
+        return make_response(jsonify({'error': 'Internal Server Error'}), 500)
+
+
+@app_views.route('/last_added_cves', methods=['GET'],
+                strict_slashes=False)
+def last_cves():
+    """ Explore the latest added CVEs including CAPEC, CWE and CPE expansions. """
+    last_cves_list = latest_30_new_cves()
+    if last_cves_list:
+        return make_response(last_cves_list, 200)
+    else:
+        return make_response(jsonify({'error': 'Internal Server Error'}), 500)
+
+
+@app_views.route('/search_cve_by_id', methods=['POST'],
+                strict_slashes=False)
+def search_cve_id():
+    """ Search our database by cve_id. """
+    if not request.is_json:
+        abort(404, 'Not a JSON')
+    supplied_cve_id = request.get_json()['cve_id']
+    ret_cve_details = search_by_cve_id(supplied_cve_id)
+    if (ret_cve_details):
+        return  make_response(ret_cve_details, 200)
+    else: # this means that the request at the model level has failed.
+        return make_response(jsonify({'error': 'Internal Server Error'}), 500)
+
+@app_views.route('/cve_db_infos', methods=['GET'],
+                strict_slashes=False)
+def current_dbs_infos():
+    """ get the current database Informations. """
+    db_result_data = get_cves_db_info()
+    if (db_result_data):
+        return make_response(db_result_data, 200)
+    else:
+        return make_response(jsonify({'error': 'Internal Server Error'}), 500)

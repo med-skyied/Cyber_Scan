82f0b1a389c8214a2dac5b1a32dc647bd947bf02
amch-med23 <skyied.med2023@gmail.com>
2024-07-12 01:14:50 +0200

    implementing the logout logic at the navBarChilLogin level

--- cyber_scan_f-e/src/components/NavBarChildLogin.jsx
+++ cyber_scan_f-e/src/components/NavBarChildLogin.jsx
@@ -2,12 +2,19 @@ import React, {Component} from "react";
 import '../App.css';
 import { Form, createBrowserRouter } from "react-router-dom";
 import cyber_scan_logo from '../assets/cyber_scan_logo.png';
+import { useNavigate } from "react-router-dom";
 
 function NavBarChildLogin(){
       // logout function.
+      const navigate = useNavigate();
       function Logout(){
         // nothing for now
         console.log('logging out...');
+        // will set the localstoreage value to 'not-_-auth'
+        // then redirect to login;
+        localStorage.setItem('user_auth_state', 'cyber_scan-not-_-auth');
+        navigate('/login');
+
       }
         return (
         <>
